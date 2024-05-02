192167ed00f3c6ca22524c79d975b381320802e3
morweee <morris920218@gmail.com>
2024-05-02 14:21:04 -0700

    fixed: main page unevenly wide game tiles

--- main_ui.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('about-link').addEventListener('click', function(event) {
-  event.preventDefault();  // Prevent the default anchor behavior
-  let aboutMessage = document.getElementById('about-message');
-  if (aboutMessage.style.display === 'block') {
-      aboutMessage.style.display = 'none';
-  } else {
-      aboutMessage.style.display = 'block';
-  }
-});
